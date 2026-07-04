00a0674a0eee437eb3e13ee0f865bbcf816ae8b5
Hanrri Kleber <hanrrikleber3666@gmail.com>
2026-07-04 20:06:27 -0300

    Adiciona geração de JSON com estatísticas

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <fstream>
 #include "../include/grafo.hpp"
 
 using namespace std;
@@ -9,67 +10,93 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n = 8;
-    int m = 12;
+    srand(time(NULL));
 
-    Grafo teste(n, "GrafoAleatorio");
+    vector<int> tamanhos_gulosos={50, 100, 500, 1000, 5000, 10000};
+    //já começam a demorar muito
+    //50000, 100000
+
+    ofstream arquivo_json("estatisticas_gulosas.json");
+    arquivo_json << "[\n";
+    bool primeiro_registro = true;
     
-    srand(time(NULL)); 
-    teste.gerar_grafo_aleatorio(n, m);
+    //testes gulosos
+    for(auto n:tamanhos_gulosos){
+        double soma_tempo=0;
 
-    cout << "Grafo gerado com " << n << " vertices e " << m << " arestas." << endl;
+        for(int i=0; i<10; i++){
+            Grafo g(n, "teste");
+            g.gerar_grafo_aleatorio(n, n*2);
 
-    vector<int> vertices_impares = teste.get_vertices_impares();
+            auto inicio=chrono::high_resolution_clock::now();
 
-    if(vertices_impares.empty()){
-        cout << "O grafo ja eh Euleriano." << endl;
-    }else{
-        vector<int> dist(teste.get_n_vertices()+1);
-        vector<int> pai(teste.get_n_vertices()+1);
-
-        vector<pair<int, pair<int, int>>> arestas_virtuais;
-
-        for(auto it:vertices_impares){
-            teste.dijkstra(it, dist, pai);
-            for(auto itt:vertices_impares){
-                if(it<itt){
-                    arestas_virtuais.push_back({dist[itt], {it, itt}});
+            vector<int> vertices_impares = g.get_vertices_impares();
+                        
+            if(!vertices_impares.empty()){
+                vector<int> dist(g.get_n_vertices()+1);
+                vector<int> pai(g.get_n_vertices()+1);
+        
+                vector<pair<int, pair<int, int>>> arestas_virtuais;
+        
+                for(auto it:vertices_impares){
+                    g.dijkstra(it, dist, pai);
+                    for(auto itt:vertices_impares){
+                        if(it<itt){
+                            arestas_virtuais.push_back({dist[itt], {it, itt}});
+                        }
+                    }
+                }
+        
+                sort(arestas_virtuais.begin(), arestas_virtuais.end());
+        
+                vector<bool> pareados(g.get_n_vertices()+1, false);
+        
+                for(auto it:arestas_virtuais){
+                    if(pareados[it.second.first] == false && pareados[it.second.second] == false){
+                        pareados[it.second.first] = true;
+                        pareados[it.second.second] = true;
+                
+                        g.dijkstra(it.second.first, dist, pai);
+        
+                        int atual=it.second.second;
+                        while(atual!=it.second.first){
+                            int anterior=pai[atual];
+                            int peso_real = g.get_peso(anterior, atual);
+                            g.adicionar_aresta(anterior, atual, peso_real);
+                            atual=anterior;
+                        }
+                    }
                 }
             }
+        
+            vector<int> ciclo = g.extrair_ciclo_euleriano(1);
+            reverse(ciclo.begin(), ciclo.end());
+
+            auto fim = chrono::high_resolution_clock::now();
+            chrono::duration<double, milli> tempo = fim-inicio;
+            soma_tempo+=tempo.count();
         }
 
-        sort(arestas_virtuais.begin(), arestas_virtuais.end());
+        double tempo_medio = soma_tempo/10.0;
+        cout<<"N = "<<n<<" | Tempo Medio: "<<tempo_medio<<" ms"<<endl;
 
-        vector<bool> pareados(teste.get_n_vertices()+1, false);
+        if(!primeiro_registro) arquivo_json << ",\n";
+        arquivo_json << "  {\n";
+        arquivo_json << "    \"n_vertices\": " << n << ",\n";
+        arquivo_json << "    \"tempo_medio_ms\": " << tempo_medio << "\n";
+        arquivo_json << "  }";
+        primeiro_registro = false;
 
-        for(auto it:arestas_virtuais){
-            if(pareados[it.second.first] == false && pareados[it.second.second] == false){
-                pareados[it.second.first] = true;
-                pareados[it.second.second] = true;
-
-                cout<<"Formou par: "<<it.second.first<<" e "<<it.second.second<<" com custo "<<it.first<<endl;
-
-                teste.dijkstra(it.second.first, dist, pai);
-
-                int atual=it.second.second;
-                while(atual!=it.second.first){
-                    int anterior=pai[atual];
-                    int peso_real = teste.get_peso(anterior, atual);
-                    teste.adicionar_aresta(anterior, atual, peso_real);
-                    atual=anterior;
-                }
-            }
+        if(tempo_medio>600000){
+            cout<<"Passou de 10 minutos. Abortado!!!"<<endl;
+            break;
         }
     }
 
-    vector<int> ciclo = teste.extrair_ciclo_euleriano(1);
-    reverse(ciclo.begin(), ciclo.end());
+    arquivo_json << "\n]\n";
+    arquivo_json.close();
 
-    cout << "circuito euleriano" << endl;
-    for(size_t i=0; i<ciclo.size(); ++i) {
-        cout<<ciclo[i]<<(i==ciclo.size()-1 ? "" : " -> ");
-    }
-    cout << endl;
+    cout << "estatística_gulosas.json gerado." << endl;
 
     return 0;
 }
